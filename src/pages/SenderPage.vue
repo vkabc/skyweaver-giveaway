2d81cb3b06ab661172f7769ea2a5752dfbe4013a
Victor Khong <victorkhong@live.com.sg>
2022-06-15 22:39:00 +0800

    fix: send transaction

--- src/pages/SenderPage.vue
+++ src/pages/SenderPage.vue
@@ -260,7 +260,7 @@ const deleteToken = (tokenID: string, e: PointerEvent) => {
           </div>
         </li>
       </ul>
-      <button type="button" @click="store.sendTransaction(receiverList, store.tokensMerged)"
+      <button type="button" @click="store.sendTransaction(store.receiverList, store.tokensMerged)"
               class="w-full font-bold text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800  rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
         Submit to Sequence!
       </button>
